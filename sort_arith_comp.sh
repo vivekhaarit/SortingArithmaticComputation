d66de18da0f3cbe1601b694fdf9bbe9bbe9413f9
vivek kumar <vivekhaarit@gmail.com>
2020-03-24 12:50:58 +0530

    [vivek]Added:read values from dictionary into array

--- sort_arith_comp.sh
+++ sort_arith_comp.sh
@@ -21,3 +21,8 @@ exprression[A*B+C]=$(( $(($a*$b))+$c))
 exprression[C+A/B]=$(($c+$(($a/$b)) ))
 exprression[A%B+C]=$(( $(($a%$b))+$c))
 
+#reading values from dictionry into array
+expr_ar[0]=${exprression[A+B*C]}
+expr_ar[1]=${exprression[A*B+C]}
+expr_ar[2]=${exprression[C+A/B]}
+expr_ar[3]=${exprression[A%B+C]}
