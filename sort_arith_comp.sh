dbd57d1bcd262942ce59427a24bf42fe7781e849
vivek kumar <vivekhaarit@gmail.com>
2020-03-24 12:43:52 +0530

    [vivek]Added:results of computation stored into dictionry

--- sort_arith_comp.sh
+++ sort_arith_comp.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 echo "welcome to arithemetic computation and sorting program"
 
@@ -13,3 +13,11 @@ echo "A+B*C: "$(($a+$(($b*$c)) ))
 echo "A*B+C: "$(( $(($a*$b))+$c)) 
 echo "C+A/B: "$(($c+$(($a/$b)) ))
 echo "A%B+C: "$(( $(($a%$b))+$c)) 
+
+#storing result of computation in dictionary
+declare -A exprression
+exprression[A+B*C]=$(($a+$(($b*$c)) ))
+exprression[A*B+C]=$(( $(($a*$b))+$c)) 
+exprression[C+A/B]=$(($c+$(($a/$b)) ))
+exprression[A%B+C]=$(( $(($a%$b))+$c))
+
